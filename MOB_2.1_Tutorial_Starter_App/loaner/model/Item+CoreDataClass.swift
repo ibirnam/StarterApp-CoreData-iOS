c392febb5530b603784779c9b70a8d9241a774c6
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2019-03-19 11:31:13 -0700

    P02

---
+++ MOB_2.1_Tutorial_Starter_App/loaner/model/Item+CoreDataClass.swift
@@ -0,0 +1,45 @@
+//
+//  Item+CoreDataClass.swift
+//  loaner
+//
+//  Created by Ian Birnam on 3/18/19.
+//  Copyright © 2019 LinnierGames. All rights reserved.
+//
+//
+
+import Foundation
+import UIKit.UIImage
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+    
+    override public func awakeFromInsert() {
+        super.awakeFromInsert()
+        
+        // Give properties initial values
+        itemTitle = "Untitled Item"
+        notes = ""
+        itemImage = UIImage(named: "no item image")!
+        loanee = Loanee(name: "", contactNumber: "")
+    }
+    
+    func assignLoanee(name: String?, phoneNumber: String?) {
+        
+        //validate contact has at least one number
+        guard let contactNumber = phoneNumber else {
+            return print("this contact needed to have at least one number")
+        }
+        
+        if let contactName = name {
+            //update loanee var
+            let newLoanee = Loanee(
+                name: contactName,
+                contactNumber: contactNumber
+            )
+            loanee = newLoanee
+        } else {
+            loanee = Loanee.init(name: "no name", contactNumber: "no contact number")
+        }
+    }
+}
---
+++ MOB_2.1_Tutorial_Starter_App/loaner/model/Item+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Item+CoreDataProperties.swift
+//  loaner
+//
+//  Created by Ian Birnam on 3/18/19.
+//  Copyright © 2019 LinnierGames. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+import UIKit.UIImage
+
+extension Item {
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        return NSFetchRequest<Item>(entityName: "Item")
+    }
+    
+    @NSManaged public var itemImage: UIImage
+    @NSManaged public var itemTitle: String
+    @NSManaged public var loanee: Loanee
+    @NSManaged public var notes: String
+}
